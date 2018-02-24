d52fd81a2d6cea98a04f0b018c1f988c11ff7118
Tinashe Chihoro <tinashechihoro@gmail.com>
2018-02-24 13:06:05 +0200

    list_display for products and category

--- shop/models.py
+++ shop/models.py
@@ -32,7 +32,7 @@ class Product(models.Model):
     image = models.ImageField(upload_to='products/%Y/%m/%d',
                               blank=True)
     description = models.TextField(blank=True)
-    price = models.DecimalField(max_digits=10,decimal_palces=2)
+    price = models.DecimalField(max_digits=10,decimal_places=2)
     stock = models.PositiveIntegerField()
     available = models.BooleanField(default=True)
     created = models.DateTimeField(auto_now_add=True)
