eeab753c2551bbda8b2de5181df355d2ab704d98
Tinashe Chihoro <tinashechihoro@gmail.com>
2018-02-24 12:56:17 +0200

    Product and Category models

--- shop/models.py
+++ shop/models.py
@@ -14,4 +14,33 @@ class Category(models.Model):
         ordering = ('name',)
         verbose_name = 'category'
         verbose_name_plural = 'categories'  
-                             
+
+    def __str__(self):
+        return self.name
+
+
+class Product(models.Model):
+    """
+    Product model
+    """   
+    category = models.ForeignKey(Category,
+                                related_name='products')
+    name  = models.CharField(max_length=200,
+                             db_index=True)
+    slug = models.SlugField(max_length=200,
+                            db_index=True)
+    image = models.ImageField(upload_to='products/%Y/%m/%d',
+                              blank=True)
+    description = models.TextField(blank=True)
+    price = models.DecimalField(max_digits=10,decimal_palces=2)
+    stock = models.PositiveIntegerField()
+    available = models.BooleanField(default=True)
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        ordering = ('name',)
+        index_together = (('id','slug'),)
+
+    def __str__(self):
+        return self.name                                                                                                                               
