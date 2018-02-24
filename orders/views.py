7213aefc49113b63dbce609481c9a8f1611c50ea
Tinashe Chihoro <tinashechihoro@gmail.com>
2018-02-24 22:04:07 +0200

    Order model

--- orders/views.py
+++ orders/views.py
@@ -1,3 +1,32 @@
 from django.shortcuts import render
+from django.db import models
+from shop.models import Product
 
-# Create your views here.
+
+class Order(models.Model):
+    """
+    Order model
+    """
+    first_name = models.CharField(max_length=50)
+    last_name = models.CharField(max_length=50)
+    email = models.EmailField()
+    address = models.CharField(max_length=250)
+    postal_code = models.CharField(max_length=20)
+    city = models.CharField(max_100)
+    created = model.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+    paid = models.BooleanField(default=False)
+
+    class Meta:
+        ordering = ('-created',)
+
+    def __str__(self):
+        return 'Ordering {}'.format(self.id)
+
+    def get_total_cost(self):
+        return sum(item.get_cost() for item in self.items.all())
+
+
+class OrderItem(models.Model):
+    """
+    """        
