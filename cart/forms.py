6ced6ae5543b0def2e63293fa752de4e34ceb6ed
Tinashe Chihoro <tinashechihoro@gmail.com>
2018-02-24 22:19:34 +0200

    Order model

--- cart/forms.py
+++ cart/forms.py
@@ -7,7 +7,9 @@ class CartAddProductForm(forms.Form):
     CartAddProductForm
     """
 
-    quantity = forms.TypeChoiceField(
+    quantity = forms.TypedChoiceField(
                                     choices=PRODUCT_QUANTITY_CHOICES,
                                     coerce=int)
-                                    
+    update = forms.BooleanField(required=False,
+                               initial=False,
+                               widget=forms.HiddenInput)
