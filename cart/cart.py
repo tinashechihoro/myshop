a86fcf2973550a0898423be5b022f69a6a517da7
Tinashe Chihoro <tinashechihoro@gmail.com>
2018-02-24 19:16:54 +0200

    add method

--- cart/cart.py
+++ cart/cart.py
@@ -17,4 +17,18 @@ class Cart(object):
         if not cart:
             #Save an empty cart in session
             cart = self.session[settings.CART_SESSION_ID] = {}
-        self.cart = cart    
+        self.cart = cart  
+
+    def add(self,product,quantity=1,updated_quantity=False):
+        """
+        Add a product to the cart or update its quantity
+        """  
+        product_id = str(product.id)
+        if product_id not in self.cart:
+            self.cart[product_id] = {'quantity':0,
+                                     'price':str(product.price)}
+        if updated_quantity:
+            self.cart[product_id]['quantity'] = quantity
+        else:
+            self.cart[product_id]['quantity'] += quantity
+        self.save()                                       
