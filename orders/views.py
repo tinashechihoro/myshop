e497b3c083e4cdc134fc27110216ca5ac81c80d5
Tinashe Chihoro <tinashechihoro@gmail.com>
2018-02-24 22:41:37 +0200

    order_create

--- orders/views.py
+++ orders/views.py
@@ -1,47 +1,23 @@
 from django.shortcuts import render
-from django.db import models
-from shop.models import Product
+from .models import OrderItem
+from .forms import OrderCreateForm
+from cart.cart import Cart
 
-
-class Order(models.Model):
-    """
-    Order model
-    """
-    first_name = models.CharField(max_length=50)
-    last_name = models.CharField(max_length=50)
-    email = models.EmailField()
-    address = models.CharField(max_length=250)
-    postal_code = models.CharField(max_length=20)
-    city = models.CharField(max_100)
-    created = model.DateTimeField(auto_now_add=True)
-    updated = models.DateTimeField(auto_now=True)
-    paid = models.BooleanField(default=False)
-
-    class Meta:
-        ordering = ('-created',)
-
-    def __str__(self):
-        return 'Ordering {}'.format(self.id)
-
-    def get_total_cost(self):
-        return sum(item.get_cost() for item in self.items.all())
-
-
-class OrderItem(models.Model):
-    """
-    OrderItem
-    """
-    order = models.ForeignKey(Order,
-                              related_name='items')
-    product = models.ForeignKey(Product,
-                                related_name='order_items')   
-    price = models.DecimalField(max_digits=10,decimal_places=2)
-    quantity = models.PositiveIntegerField(default=1)
-
-    def __str__(self):
-        return '{}'.format(self.id)
-
-    def get_cost(self):
-        return self.price * self.quantity
-            
-
+def order_create(request):
+    cart = cart(request)
+    if request.method == 'POST':
+        form = OrderCreateForm(request.POST)
+        if form.is_valid():
+            order = form.save()
+            for item in cart:
+                OrderItem.objects.create(order=order,
+                                        product=item['product'],
+                                        price=item['price'],
+                                        quantity=item['quantity'])
+            cart.clear()    
+        return render(request,'orders/order/create.html',
+                               {'order':order})
+    else:
+        form = OrderCreateForm()
+    return render(request,'orders/order/create.html',
+                           {'cart':cart,'form':form})                                                           
