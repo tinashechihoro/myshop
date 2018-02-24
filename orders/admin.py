b26c730dd186c55943a1c0a06ea312861088f2cc
Tinashe Chihoro <tinashechihoro@gmail.com>
2018-02-24 22:27:09 +0200

    Order model in admin

--- orders/admin.py
+++ orders/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
+from .models import Order,OrderItem
 
-# Register your models here.
+class OrderItemInline(admin.TabularInLine):
+    model = OrderItem
+    raw_id_fields = ['product']
+
+class OrderAdmin(admin.ModelAdmin):
+    list_display = ['id','first_name','email',
+                    'address','postal_code','city',
+                    'paid','created','updated'] 
+    list_filter = ['paid','created','updated']
+admin.site.register(Order,OrderAdmin)                    
+
+
--- orders/models.py
+++ orders/models.py
@@ -12,8 +12,8 @@ class Order(models.Model):
     email = models.EmailField()
     address = models.CharField(max_length=250)
     postal_code = models.CharField(max_length=20)
-    city = models.CharField(max_100)
-    created = model.DateTimeField(auto_now_add=True)
+    city = models.CharField(max_length=100)
+    created = models.DateTimeField(auto_now_add=True)
     updated = models.DateTimeField(auto_now=True)
     paid = models.BooleanField(default=False)
 
