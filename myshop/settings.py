38b2e158f0712c64168e682f0cdb47c2056009cb
Tinashe Chihoro <tinashechihoro@gmail.com>
2018-02-24 18:40:44 +0200

    'material.admin',     'material',

--- myshop/settings.py
+++ myshop/settings.py
@@ -31,6 +31,8 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'material.admin',
+    'material',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
