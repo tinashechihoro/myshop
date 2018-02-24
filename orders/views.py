01793605531f9aef1bd3b0b08372e6342dadb65f
Tinashe Chihoro <tinashechihoro@gmail.com>
2018-02-24 22:09:05 +0200

    get cost

--- orders/views.py
+++ orders/views.py
@@ -29,4 +29,19 @@ class Order(models.Model):
 
 class OrderItem(models.Model):
     """
-    """        
+    OrderItem
+    """
+    order = models.ForeignKey(Order,
+                              related_name='items')
+    product = models.ForeignKey(Product,
+                                related_name='order_items')   
+    price = models.DecimalField(max_digits=10,decimal_places=2)
+    quantity = models.PositiveIntegerField(default=1)
+
+    def __str__(self):
+        return '{}'.format(self.id)
+
+    def get_cost(self):
+        return self.price * self.quantity
+            
+
